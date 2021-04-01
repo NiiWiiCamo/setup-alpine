e84f51d8542b396fdf1885020feed67e18260f27
NiiWiiCamo <mail@nilsblume.de>
2021-04-01 20:02:02 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,14 +15,21 @@ apk add bash curl iptables ip6tables htop nano open-vm-tools screen sudo vlan
 sed -i 's/\/bin\/ash/\/bin\/bash/g' /etc/passwd
 
 ## setup skel and cron folders
-mkdir -p /etc/skel/
-mkdir -p /etc/skel/.cronjobs/periodic/15min
-mkdir -p /etc/skel/.cronjobs/periodic/hourly
-mkdir -p /etc/skel/.cronjobs/periodic/4aday
-mkdir -p /etc/skel/.cronjobs/periodic/daily
-mkdir -p /etc/skel/.cronjobs/periodic/monthly
+mkdir -p /etc/skel
+mkdir -p /etc/skel/.cronjobs/15min
+mkdir -p /etc/skel/.cronjobs/hourly
+mkdir -p /etc/skel/.cronjobs/4aday
+mkdir -p /etc/skel/.cronjobs/daily
+mkdir -p /etc/skel/.cronjobs/monthly
 mkdir -p /etc/skel/.cronjobs/startup
-mkdir -p /etc/startup
+
+mkdir -p /etc/periodic
+mkdir -p /etc/periodic/15min
+mkdir -p /etc/periodic/hourly
+mkdir -p /etc/periodic/4aday
+mkdir -p /etc/periodic/daily
+mkdir -p /etc/periodic/monthly
+mkdir -p /etc/periodic/startup
 
 ## add screen autoconnect
 cat <<EOF > /etc/skel/.bash_profile
@@ -43,12 +50,12 @@ mkdir -p /etc/skel/.ssh
 cat <<EOF > /etc/skel/usercron
 # do daily/weekly/monthly maintenance
 # min	hour	day	month	weekday	command
-*/15	*	*	*	*	run-parts ~/.cronjobs/periodic/15min
-0	*	*	*	*	run-parts ~/.cronjobs/periodic/hourly
-0	*/6	*	*	*	run-parts ~/.cronjobs/periodic/4aday
-0	2	*	*	*	run-parts ~/.cronjobs/periodic/daily
-0	3	*	*	6	run-parts ~/.cronjobs/periodic/weekly
-0	5	1	*	*	run-parts ~/.cronjobs/periodic/monthly
+*/15	*	*	*	*	run-parts ~/.cronjobs/15min
+0	*	*	*	*	run-parts ~/.cronjobs/hourly
+0	*/6	*	*	*	run-parts ~/.cronjobs/4aday
+0	2	*	*	*	run-parts ~/.cronjobs/daily
+0	3	*	*	6	run-parts ~/.cronjobs/weekly
+0	5	1	*	*	run-parts ~/.cronjobs/monthly
 @reboot					run-parts ~/.cronjobs/startup
 
 # custom cronjobs:
@@ -64,7 +71,7 @@ cat <<EOF > rootcron
 0	2	*	*	*	run-parts /etc/periodic/daily
 0	3	*	*	6	run-parts /etc/periodic/weekly
 0	5	1	*	*	run-parts /etc/periodic/monthly
-@reboot					run-parts /etc/startup
+@reboot					run-parts /etc/periodic/startup
 
 # custom cronjobs:
 EOF
@@ -81,7 +88,7 @@ echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
 
 
 ## execute as new user
-su $newuser -c "cd ~; curl -sSL https://raw.githubusercontent.com/NiiWiiCamo/ssh/master/get-keys.bash | tee ~/.cronjobs/periodic/4aday/get-ssh-keys | bash; chmod +x ~/.cronjobs/periodic/4aday/*; crontab ~/usercron; rm ~/usercron"
+su $newuser -c "cd ~ && curl -sSL https://raw.githubusercontent.com/NiiWiiCamo/ssh/master/get-keys.bash | tee ~/.cronjobs/4aday/get-ssh-keys | tee ~/.cronjobs/startup/get-ssh-keys | bash ; chmod +x ~/.cronjobs/4aday/* ; chmod +x ~/.cronjobs/startup/* ; crontab ~/usercron && rm ~/usercron"
 
 ## setup sshd_config
 sed -i 's/\#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
